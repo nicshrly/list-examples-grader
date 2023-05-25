ff6cfee686ccbf6aa08717c2b784532d8fc381ae
nicshrly <130081492+nicshrly@users.noreply.github.com>
2023-05-25 09:35:13 -0700

    added stuff

--- grade.sh
+++ grade.sh
@@ -11,7 +11,7 @@ echo 'Finished cloning'
 files=$(find student-submission/*.java)
 for file in $files
 do
-    if [[ -f $file]] && [[ $file == student-submission/ListExamples.java ]]
+    if [[ -f $file ]] && [[ $file == student-submission/ListExamples.java ]]
         then
             echo 'submitted correct file'
     else
@@ -29,10 +29,16 @@ javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java > output.txt
 if [[ $? -ne 0 ]]
     then exit
 fi
-java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > output.txt 2>&1
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt 2>&1
 
-if [[ grep "OK" output.txt ]]
+# if [[ grep "OK" output.txt ]]
 
+head -n 2 output.txt > result.txt
+tail -n 1 result.txt > result1.txt
+
+#if [[   output.txt ]]
+
+#sed -n "2p" output.txt > result.txt
 
 
 # Draw a picture/take notes on the directory structure that's set up after
